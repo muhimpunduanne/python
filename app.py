af1d6966e065a0e219a750165420bed09aa5e8b3
MUHIMPUNDU Anne <muhimpunduan@gmail.com>
2026-03-17 10:57:36 +0200

    variable declaration

---
+++ app.py
@@ -0,0 +1,20 @@
+print("Hello world")
+print("*" * 10)
+
+Student_Count = 1000
+
+name ="anne"
+
+Is_Published = True
+
+# print(Student_Count)
+
+course = "python programming"
+# print (len(course)) this is going to print p
+
+print(course[0])
+# print(course[-1]) this is going to print g it can last character of the sentence
+print(course[0:3])
+print(course[:3])
+print(course[0:])
+print(course[:])
