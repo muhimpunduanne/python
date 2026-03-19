c210234b7c03d5898c2097099ced7e3313822fc7
MUHIMPUNDU Anne <muhimpunduan@gmail.com>
2026-03-19 17:30:21 +0200

    calculator project

---
+++ cal.py
@@ -0,0 +1,28 @@
+num1=float(input("Enter the first number: "))
+num2=float(input("Enter the second number: "))
+
+print("choose operation")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice= input("Enter the choice(1/2/3/4):")
+
+if choice=='1':
+    print("Result:",num1 + num2)
+elif choice=='2':
+    print("Result:",num1-num2)
+elif choice == '3':
+    print("Result:", num1 * num2)
+elif choice == '4':
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error: Cannot divide by zero")
+
+else:
+    print("Invalid input")    
+
+
+print(choice)    
